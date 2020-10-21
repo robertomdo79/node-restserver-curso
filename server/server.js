312a1ac29f0dc3ff6f4db1779264fd63509500ae
Roberto Montes de Oca <robbiemdo79@gmail.com>
2020-10-21 13:18:03 -0400

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 
-const { request } = require('express');
+const mongoose = require('mongoose');
 const express = require('express');
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -10,42 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-});
-
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            body
-        });
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+  useFindAndModify: true,
+  useCreateIndex: true
+}, (err) => {
+    if(err){
+        throw err;
     }
-
-
-});
- 
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
